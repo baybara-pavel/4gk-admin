78a810c8d9ff53475a89df020502f4766185203d
buybara-pavel <baybara.pavel@gmail.com>
2015-05-10 22:20:31 +0600

    first commit

---
+++ tars/tasks/css/compile-css.js
@@ -0,0 +1,128 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var gutil = require('gulp-util');
+var gulpif = require('gulp-if');
+var autoprefixer = require('gulp-autoprefixer');
+var replace = require('gulp-replace-task');
+var addsrc = require('gulp-add-src');
+var notify = require('gulp-notify');
+var tarsConfig = require('../../../tars-config');
+var notifier = require('../../helpers/notifier');
+var browserSync = require('browser-sync');
+
+var useAutoprefixer = false;
+var helperStream;
+var mainStream;
+var ie9Stream;
+
+if (tarsConfig.autoprefixerConfig) {
+    useAutoprefixer = true;
+}
+
+var scssFilesToConcatinate = [
+        './markup/' + tarsConfig.fs.staticFolderName + '/scss/normalize.scss',
+        './markup/' + tarsConfig.fs.staticFolderName + '/scss/libraries/**/*.scss',
+        './markup/' + tarsConfig.fs.staticFolderName + '/scss/libraries/**/*.css',
+        './markup/' + tarsConfig.fs.staticFolderName + '/scss/mixins.scss',
+        './markup/' + tarsConfig.fs.staticFolderName + '/scss/sprites-scss/sprite_96.scss'
+    ];
+
+if (tarsConfig.useSVG) {
+    scssFilesToConcatinate.push(
+        './markup/' + tarsConfig.fs.staticFolderName + '/scss/sprites-scss/svg-sprite.scss'
+    );
+}
+
+scssFilesToConcatinate.push(
+    './markup/' + tarsConfig.fs.staticFolderName + '/scss/fonts.scss',
+    './markup/' + tarsConfig.fs.staticFolderName + '/scss/vars.scss',
+    './markup/' + tarsConfig.fs.staticFolderName + '/scss/GUI.scss',
+    './markup/' + tarsConfig.fs.staticFolderName + '/scss/common.scss',
+    './markup/' + tarsConfig.fs.staticFolderName + '/scss/plugins/**/*.scss',
+    './markup/' + tarsConfig.fs.staticFolderName + '/scss/plugins/**/*.css',
+    './markup/modules/*/*.scss'
+);
+
+/**
+ * Scss compilation
+ * @param  {object} buildOptions
+ */
+module.exports = function (buildOptions) {
+
+    var patterns = [];
+
+    patterns.push(
+        {
+            match: '%=staticPrefixForCss=%',
+            replacement: tarsConfig.staticPrefixForCss()
+        }
+    );
+
+    return gulp.task('css:compile-css', function () {
+
+        helperStream = gulp.src(scssFilesToConcatinate);
+        mainStream = helperStream.pipe(addsrc.append('./markup/' + tarsConfig.fs.staticFolderName + '/scss/etc/**/*.scss'));
+        ie9Stream = helperStream.pipe(
+                                addsrc.append([
+                                        './markup/modules/*/ie/ie9.scss',
+                                        './markup/' + tarsConfig.fs.staticFolderName + '/scss/etc/**/*.scss'
+                                    ])
+                            );
+
+        mainStream
+            .pipe(concat('main' + buildOptions.hash + '.css'))
+            .pipe(replace({
+                patterns: patterns,
+                usePrefix: false
+            }))
+            .pipe(sass({
+                    errLogToConsole: false,
+                    onError: function (error) {
+                        notify().write('\nAn error occurred while compiling css.\nLook in the console for details.\n');
+                        return gutil.log(gutil.colors.red(error.message + ' on line ' + error.line + ' in ' + error.file));
+                    }
+                }))
+            .pipe(
+                gulpif(useAutoprefixer,
+                    autoprefixer(
+                        {
+                            browsers: tarsConfig.autoprefixerConfig,
+                            cascade: true
+                        }
+                    )
+                )
+            )
+            .on('error', notify.onError(function (error) {
+                return '\nAn error occurred while autoprefixing css.\nLook in the console for details.\n' + error;
+            }))
+            .pipe(gulp.dest('./dev/' + tarsConfig.fs.staticFolderName + '/css/'))
+            .pipe(browserSync.reload({ stream: true }))
+            .pipe(
+                notifier('Scss-files\'ve been compiled')
+            );
+
+        return ie9Stream
+            .pipe(concat('main_ie9' + buildOptions.hash + '.css'))
+            .pipe(replace({
+                patterns: patterns,
+                usePrefix: false
+            }))
+            .pipe(sass({
+                errLogToConsole: false,
+                onError: function (error) {
+                    notify().write('\nAn error occurred while compiling css for ie9.\nLook in the console for details.\n');
+                    return gutil.log(gutil.colors.red(error.message + ' on line ' + error.line + ' in ' + error.file));
+                }
+            }))
+            .pipe(autoprefixer('ie 9', { cascade: true }))
+            .on('error', notify.onError(function (error) {
+                return '\nAn error occurred while autoprefixing css.\nLook in the console for details.\n' + error;
+            }))
+            .pipe(gulp.dest('./dev/' + tarsConfig.fs.staticFolderName + '/css/'))
+            .pipe(browserSync.reload({ stream: true }))
+            .pipe(
+                notifier('Css-files for ie9 have been compiled')
+            );
+    });
+};
---
+++ tars/tasks/css/make-fallback-for-svg.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp');
+var spritesmith = require('gulp.spritesmith');
+var notify = require('gulp-notify');
+var gutil = require('gulp-util');
+var tarsConfig = require('../../../tars-config');
+var notifier = require('../../helpers/notifier');
+
+/**
+ * Make sprite for svg-fallback and scss for this sprite
+ * Return pipe with scss for sprite
+ * @param  {object} buildOptions
+ */
+module.exports = function (buildOptions) {
+
+    return gulp.task('css:make-fallback-for-svg', function (cb) {
+        var spriteData = '';
+
+        if (tarsConfig.useSVG && gutil.env.ie8) {
+
+            spriteData = gulp.src('./dev/' + tarsConfig.fs.staticFolderName + '/' + tarsConfig.fs.imagesFolderName + '/rastered-svg-images/*.png')
+                .pipe(
+                    spritesmith(
+                        {
+                            imgName: 'svg-fallback-sprite.png',
+                            cssName: 'svg-fallback-sprite.scss',
+                            Algorithms: 'diagonal',
+                            cssTemplate: './markup/' + tarsConfig.fs.staticFolderName + '/scss/sprite-generator-templates/scss.svg-fallback-sprite.mustache'
+                        }
+                    )
+                )
+                .on('error', notify.onError(function (error) {
+                    return '\nAn error occurred while making fallback for svg.\nLook in the console for details.\n' + error;
+                }));
+
+            spriteData.img.pipe(gulp.dest('./dev/' + tarsConfig.fs.staticFolderName + '/' + tarsConfig.fs.imagesFolderName + '/rastered-svg-sprite/'))
+                .pipe(
+                    notifier('Sprite img for svg is ready')
+                );
+
+            return spriteData.css.pipe(gulp.dest('./markup/' + tarsConfig.fs.staticFolderName + '/scss/sprites-scss/'))
+                    .pipe(
+                        notifier('Scss for svg-sprite is ready')
+                    );
+
+        } else {
+            gutil.log('!Svg-fallback is not used!');
+            cb(null);
+        }
+    });
+};
